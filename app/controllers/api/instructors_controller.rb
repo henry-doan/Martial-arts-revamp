c0725149e3e06afa522a08d32f1c464d6b6a10fc
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-10-02 20:13:26 -0600

    controllers for instructors and programs

--- app/controllers/api/instructors_controller.rb
+++ app/controllers/api/instructors_controller.rb
@@ -1,2 +1,36 @@
 class Api::InstructorsController < ApplicationController
+
+    def index
+        render json: Instructor.all
+    end
+
+    def create
+        @instructor = Instructor.new(instructor_params)
+        if @instructor.save
+            render json: @instructor
+        else
+            render json: {errors: @instructor.errors}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @instructor = Instructor.find(params[:id])
+        if @instructor.update(instructor_params)
+            render json: @instructor
+        else
+            render json: {errors: @instructor.errors}, status: :unprocessable_entity
+        end 
+    end
+
+    def destroy
+        Instructor.find(params[:id]).destroy
+        render json: {message: 'instructor deleted'}
+    end
+
+    private
+    def instructor_params
+        params.require(:instructor).permit(:first_name, :last_name, :specialty, :years, 
+        :experience, :sm_facebook, :sm_linkedin, :sm_twitter, :sm_instagram, :sm_youtube)
+    end
+
 end
--- app/controllers/api/programs_controller.rb
+++ app/controllers/api/programs_controller.rb
@@ -1,2 +1,35 @@
 class Api::ProgramsController < ApplicationController
+
+    def index
+        render json: Program.all
+    end
+
+    def create
+        @program = Program.new(program_params)
+        if @program.save
+            render json: @program
+        else
+            render json: {errors: @program.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @program = Program.find(params[:id])
+        if @program.update(program_params)
+            render json: @program_params
+        else
+            render json: {errors: @program.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        Program.find(params[:id]).destroy
+        render json: {message: 'Program deleted'}
+    end
+
+    private
+    def program_params
+    params.require(:program).permit(:title, :description, :image, :featured_boolean, :gallery )
+    end
+
 end
