f252cf444ae44f7afa54568da034c882c9ff7271
Koki Johnson <koki.johson@gmail.com>
2019-10-02 20:17:54 -0600

    finished contact and about model

--- app/controllers/api/abouts_controller.rb
+++ app/controllers/api/abouts_controller.rb
@@ -1,2 +1,35 @@
 class Api::AboutsController < ApplicationController
+  def index 
+    render json: Content.all
+  end
+
+  def create 
+    @content = Content.new(content_params)
+    if @content.save
+      render json: @content
+    else
+      render json: { errors: @content.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @content = Content.find(params[:id])
+    if @content.update(content_params)
+      render json: @content
+    else
+      render json: { errors: @content.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Content.find(params[:id]).destroy
+    render json: { message: 'content deleted' }
+  end
+
+  private
+    def crud_params
+      # {post: { title: 'food', body: 'yummy'}}
+      # { post: {title: '', body: ''} }
+      params.require(:crud).permit(:title, :body)
+    end
 end
--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,36 @@
 class Api::ContactsController < ApplicationController
 end
+    render json: Contact.all
+  end
+
+  def create 
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      render json: @contact
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Contact.find(params[:id]).destroy
+    render json: { message: 'contact deleted' }
+  end
+
+  private
+    def post_params
+      # {post: { title: 'food', body: 'yummy'}}
+      # { post: {title: '', body: ''} }
+      params.require(:contact).permit(:name, :email, :phone, :interest, :note)
+      
+    end
+end
