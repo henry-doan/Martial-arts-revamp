5e7ddcd726e6f71b33a884a53ac6bb488d26c8fc
Henry Doan <henrydoan96@gmail.com>
2019-10-08 20:08:40 -0600

    Update ProgramShow.js

--- client/src/components/Programs/ProgramShow.js
+++ client/src/components/Programs/ProgramShow.js
@@ -1,32 +1,24 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 class ProgramShow extends Component {
-
   render(){
     const { id, title, description, image, gallery, featured_boolean, updateProgram, deleteProgram } = this.props
     return (
-
       <div>
-
-      <h1>id: {id}<br />
-      Title: {title}<br />
-      Description: {description}<br />
-      <Link to={{
-        pathname: "/programForm",
-        state: { id, title, description, image, gallery, featured_boolean }
-      }}>Edit Program</Link>
-      <button onClick = {() => deleteProgram(id)}>delete</button>
-      </h1><br />
-
-
+        <h1>id: {id}<br />
+        Title: {title}<br />
+        Description: {description}<br />
+        <Link to={{
+          pathname: "/programForm",
+          state: { id, title, description, image, gallery, featured_boolean }
+        }}>Edit Program</Link>
+        <button onClick = {() => deleteProgram(id)}>delete</button>
+        </h1>
+        <br />
       </div>
-
     )
   }
 }
-
 
 export default ProgramShow;
