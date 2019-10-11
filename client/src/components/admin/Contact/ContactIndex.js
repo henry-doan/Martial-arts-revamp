0666aabbbb111b146be04b579eba04571030e882
Koki Johnson <koki.johson@gmail.com>
2019-10-10 18:57:09 -0600

    added index and show for contact and walkin

---
+++ client/src/components/admin/Contact/ContactIndex.js
@@ -0,0 +1,21 @@
+import React, {Component} from './node_modules/react';
+import WalkinShow from "./WalkinShow";
+import {Link} from './node_modules/react-router-dom';
+
+const WalkinIndex = ({ walkins, updateWalkin, addWalkin, }) => (
+  <WalkinConsumer>
+    { value => (
+        <>
+          { value.walkins.map(walkin =>
+            <WalkinShow
+              key={walkin.id}
+              {...walkin}
+              updateWalkin={value.updateWalkin}
+            />
+          )}
+          <Link to="/walkinForm">Add new Walkin</Link>
+        </>
+      )}
+  </WalkinConsumer>
+)
+export default WalkinIndex;
