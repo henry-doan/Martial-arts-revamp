41d80e33393fd1de8fc682ca5a3fa6161384322e
Henry Doan <henrydoan96@gmail.com>
2019-10-24 15:09:28 -0600

    Update HomeProgramStyles.js

--- client/src/components/styledComponents/HomeProgramStyles.js
+++ client/src/components/styledComponents/HomeProgramStyles.js
@@ -40,29 +40,6 @@ transform: translate(-50%, -50%);
 text-shadow: 0px 0px 8px #000;
 `;
 
-// export const PButton = styled.button`
-// color:black;
-// padding:0.3em 1.2em;
-// margin:0 0.3em 0.3em 0;
-// text-decoration:none;
-// font-size:1em;
-// position: absolute;
-// z-index: 888;
-// margin: 0 auto;
-// text-align:center;
-// bottom: 30%;
-// left:50%;
-// -ms-transform: translate(-50%, -50%);
-// transform: translate(-50%, -50%);
-
-//   &:hover{
-//   font-size:2em;
-//   border-radius:50px !important;
-//   color:orange !important;
-// }
-
-// `;
-
 export const HeroProgramImage = styled.img`
 width: 100%;
 `;
