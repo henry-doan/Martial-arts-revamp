6ff33b555ba06391869c534e92501b8bb75c7afc
Henry Doan <henrydoan96@gmail.com>
2019-10-23 20:06:16 -0600

    Update WalkinIndex.js

--- client/src/components/admin/walkin/WalkinIndex.js
+++ client/src/components/admin/walkin/WalkinIndex.js
@@ -1,26 +1,3 @@
-// import React, {Component} from 'react';
-// import WalkinShow from "./WalkinShow";
-// import {Link} from 'react-router-dom';
-// import WalkinConsumer from '../../../providers/WalkinProvider';
-
-// const WalkinIndex = ({ walkins, updateWalkin, addWalkin, }) => (
-//   <WalkinConsumer>
-//     { value => (
-//         <>
-//           { value.walkins.map(walkin =>
-//             <WalkinShow
-//               key={walkin.id}
-//               {...walkin}
-//               updateWalkin={value.updateWalkin}
-//             />
-//           )}
-//           <Link to="/walkinForm">Add new Walkin</Link>
-//         </>
-//       )}
-//   </WalkinConsumer>
-// )
-// export default WalkinIndex;
-
 import React, {Component} from 'react';
 import ContactShow from "./ContactShow";
 import {Link} from "react-router-dom";
